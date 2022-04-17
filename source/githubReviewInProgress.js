8e1b8f99ae0ab4300f516df36f2522431dad969b
Domantas Petrauskas <domantas@zyro.com>
2022-04-17 10:33:53 +0300

    Authenticate with GitHub API

--- source/githubReviewInProgress.js
+++ source/githubReviewInProgress.js
@@ -1,15 +1,27 @@
 import optionsStorage from './optionsStorage.js';
 
 async function init() {
-	const options = await optionsStorage.getAll();
-	const color = 'rgb(' + options.colorRed + ', ' + options.colorGreen + ',' + options.colorBlue + ')';
-	const text = options.text;
-	const notice = document.createElement('div');
-	notice.innerHTML = text;
-	document.body.append(notice);
-	notice.id = 'text-notice';
-	notice.style.border = '2px solid ' + color;
-	notice.style.color = color;
+	const {personalToken} = await optionsStorage.getAll();
+
+	const githubGraphqlUrl = 'https://api.github.com/graphql';
+
+	fetch(githubGraphqlUrl, {
+		method: 'POST',
+		headers: {
+			Authorization: `bearer ${personalToken}`,
+		},
+		body: JSON.stringify({
+			query: `
+				query {
+					viewer {
+						login
+					}
+				}
+			`,
+		}),
+	})
+		.then(response => response.json())
+		.then(result => console.log(result));
 }
 
 init();
